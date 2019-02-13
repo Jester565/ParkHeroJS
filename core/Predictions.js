e3d87ffca6c7107a21c614fd245f4afdc09ca69e
Jester565 <ajcraig543@gmail.com>
2019-02-12 23:43:46 -0800

    Added ride prediction getters to Predictions.js

--- core/Predictions.js
+++ core/Predictions.js
@@ -1,3 +1,24 @@
+
+//Convert array to map given an array element's key
+function indexArray(arrMap, arr, idxName, valName = null) {
+    for (var result of arr) {
+        var obj = arrMap[result[idxName]];
+        if (obj == null) {
+            arrMap[result[idxName]] = [];
+            obj = arrMap[result[idxName]];
+        }
+        if (obj != null) {
+            delete result[idxName];
+            if (valName == null) {
+                obj.push(result);
+            } else {
+                obj.push(result[valName]);
+            }
+        }
+    }
+    return arrMap;
+}
+
 //TODO: May have to handle unpredictable fpt values after fast passes expire
 async function getFastPassPrediction(rideID, dateTime, query) {
     var firstDT = dateTime.clone().set({minute: 0, second: 0, millisecond: 0});
@@ -31,6 +52,69 @@ async function getFastPassPrediction(rideID, dateTime, query) {
     return predTime;
 }
 
+//Get average of all prediction information for the rest of the day at a resort
+async function getPredictTimeHeuristics(tz, query) {
+    var now = moment().tz(tz);
+    var nowStr = now.format("YYYY-MM-DD HH:mm:ss");
+    var results =await query(`SELECT br.rideID AS rideID, AVG(br.waitMins) AS avgWaitMins, MIN(br.waitMins) AS minWaitMins, MAX(br.waitMins) AS maxWaitMins 
+        FROM BatchResults br, ParkSchedules ps
+        WHERE (br.doy=(MONTH(ps.date)*31 + DAYOFMONTH(ps.date)) AND br.year=YEAR(ps.date) 
+        AND br.hour>=(HOUR(?) + DATEDIFF(DATE(?), ps.date) * 24 - br.openHour)
+        AND ps.date=DATE(DATE_SUB(?, INTERVAL 4 HOUR))) GROUP BY br.rideID`, 
+        [nowStr, nowStr, nowStr]);
+    return indexArray({}, results, "rideID");
+}
+
+//Get upcoming prediction times for the next x hours
+async function getPredictTime(tz, hourOffset, query) {
+    var dateTime = moment().tz(tz);
+    dateTime.add(hourOffset, 'hour');
+    var dtStr = now.format("YYYY-MM-DD HH:mm:ss");
+    var results = await query(`SELECT br.rideID AS rideID, br.waitMins AS waitMins FROM BatchResults br, ParkSchedules ps
+        WHERE (br.doy=(MONTH(ps.date)*31 + DAYOFMONTH(ps.date)) AND br.year=YEAR(ps.date) 
+        AND br.hour=(HOUR(?) + DATEDIFF(DATE(?), ps.date) * 24 - br.openHour)
+        AND ps.date=DATE(DATE_SUB(?, INTERVAL 4 HOUR)))`, 
+        [dtStr, dtStr, dtStr]);
+    return indexArray({}, results, "rideID");
+}
+
+//Calculates the waitRating from distance from prediction and predicted trends for wait time
+function getWaitRating(waitTime, avgWaitMins, minWaitMins, maxWaitMins, firstPredictTime, secondPredictTime) {
+    var waitRating = null;
+    var predictTime = null;
+    var getTime = new Date();
+    if (waitTime != null) {
+        if (firstPredictTime != null && secondPredictTime != null) {
+            predictTime = ((60.0 - getTime.getMinutes())/60.0) * firstPredictTime + (getTime.getMinutes()/60.0) * secondPredictTime;
+        } else if (firstPredictTime != null) {
+            predictTime = firstPredictTime;
+        }
+        if (predictTime != null) {
+            waitRating = 0;
+            if (Math.abs(predictTime - waitTime) != 0) {
+                waitRating = Math.pow(Math.abs(predictTime - waitTime), 1.3) * ((predictTime) - (waitTime)) / Math.abs((predictTime) - (waitTime));
+            }
+           
+            if (maxWaitMins - minWaitMins > 5.0) {
+                waitRating /= (Math.sqrt((maxWaitMins - minWaitMins)/2) * 1.5);
+            } else {
+                waitRating /= 5.0;
+            }
+            waitRating += 10;
+            waitRating /= 2;
+            if (waitRating > 10) {
+                waitRating = 10;
+            } else if (waitRating < 0) {
+                waitRating = 0;
+            }
+        }
+    }
+    return waitRating;
+}
+
 module.exports = {
-    getFastPassPrediction: getFastPassPrediction
+    getFastPassPrediction: getFastPassPrediction,
+    getPredictTimeHeuristics: getPredictTimeHeuristics,
+    getPredictTime: getPredictTime,
+    getWaitRating: getWaitRating
 };
